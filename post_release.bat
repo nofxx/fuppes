10669b875a1c93a47e7a5258f551c57ab9806f8c
schnitzler <schnitzler@92f15cf5-d3f9-0310-bf6d-83d55f1f8be4>
2005-06-26 10:46:54 +0000

    - Added copying files from root to setup folder

--- post_release.bat
+++ post_release.bat
@@ -1,1 +1,6 @@
 xcopy .\Release\fuppes.exe .\setup\ /Y
+xcopy .\AUTHORS            .\setup\ /Y
+xcopy .\ChangeLog          .\setup\ /Y
+xcopy .\COPYING            .\setup\ /Y
+xcopy .\fuppes.cfg.example .\setup\ /Y
+xcopy .\README             .\setup\ /Y
