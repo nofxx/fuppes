c6b76a14cb6b0916f30fca8076a08ab857171608
voelkel <voelkel@92f15cf5-d3f9-0310-bf6d-83d55f1f8be4>
2007-01-21 16:00:19 +0000

    - UPnP events

---
+++ src/GENA/EventNotification.cpp
@@ -0,0 +1,60 @@
+/***************************************************************************
+ *            EventNotification.cpp
+ *
+ *  FUPPES - Free UPnP Entertainment Service
+ *
+ *  Copyright (C) 2007 Ulrich Völkel <u-voelkel@users.sourceforge.net>
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "EventNotification.h"
+
+#include "../Common/Common.h"
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+std::string CEventNotification::BuildHeader()
+{
+  stringstream sHeader;
+  
+  sHeader << 
+    "NOTIFY / HTTP/1.1\r\n" <<  
+    "HOST: " << m_sHost << "\r\n" <<
+    "CONTENT-TYPE: text/xml\r\n" <<
+    "CONTENT-LENGTH: " << m_sContent.length() << "\r\n" <<
+    "NT: upnp:event\r\n" <<
+    "NTS: upnp:propchange\r\n" <<
+    "SID: uuid:" << m_sSID << "\r\n" <<
+    "SEQ: 0\r\n" <<
+    "User-Agent: SSDP CD Events\r\n" << 
+    "Cache-Control: no-cache\r\n\r\n";
+  
+  return sHeader.str();
+}
+
+void CEventNotification::SetCallback(std::string p_sCallback)
+{
+  cout << __FILE__ << endl;
+  
+  cout << "CALLBACK: " << p_sCallback << endl;
+  
+  SplitURL(p_sCallback, &m_sSubscriberIP, &m_nSubscriberPort);
+  
+  cout << "IP: " << m_sSubscriberIP << endl;
+  cout << "Port: " << m_nSubscriberPort << endl;
+}
