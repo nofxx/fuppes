6a714f89204abfd5a33bf398b89126f1ac4a66e3
voelkel <voelkel@92f15cf5-d3f9-0310-bf6d-83d55f1f8be4>
2007-02-18 03:50:06 +0000

    - iconv checks

--- src/Common/Common.cpp
+++ src/Common/Common.cpp
@@ -42,6 +42,12 @@
 #include <fcntl.h>
 #include <iconv.h>
 
+#ifdef HAVE_CONFIG_H
+#include "../../config.h"
+#else
+#define ICONV_SECOND_ARG const char**
+#endif
+
 using namespace std;
 
 /*===============================================================================
@@ -435,7 +441,7 @@ std::string ToUTF8(std::string p_sValue, std::string p_sEncoding)
   memset(szOutBuf, 0, p_sValue.length() * 2 + 1);
   
   //#ifdef WIN32
-  iconv(icv, (const char**)&szInBuf, &nInbytes, &pOutBuf, &nOutbytes);
+  iconv(icv, (ICONV_SECOND_ARG)&szInBuf, &nInbytes, &pOutBuf, &nOutbytes);
   //#else
   //iconv(icv, &szInBuf, &nInbytes, &pOutBuf, &nOutbytes);  
   //#endif
