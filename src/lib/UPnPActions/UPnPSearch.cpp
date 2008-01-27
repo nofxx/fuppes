afd3a3066f6ed75ba59a12675b986c0e9ce14be4
voelkel <voelkel@92f15cf5-d3f9-0310-bf6d-83d55f1f8be4>
2008-01-27 10:20:52 +0000

    - fixed #1880069

--- src/lib/UPnPActions/UPnPSearch.cpp
+++ src/lib/UPnPActions/UPnPSearch.cpp
@@ -3,13 +3,14 @@
  *
  *  FUPPES - Free UPnP Entertainment Service
  *
- *  Copyright (C) 2007 Ulrich Völkel <u-voelkel@users.sourceforge.net>
+ *  Copyright (C) 2007-2008 Ulrich Völkel <u-voelkel@users.sourceforge.net>
  ****************************************************************************/
 
 /*
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as 
- *  published by the Free Software Foundation.
+ *  it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -211,6 +212,10 @@ std::string CUPnPSearch::BuildSQL(bool p_bCount)
 				  sProp = "d.A_ARTIST";
 					bNumericProp = false;
 				}
+				else if(sProp.compare("upnp:genre") == 0) {
+				  sProp = "d.A_GENRE";
+					bNumericProp = false;
+				}
         else if(sProp.compare("res:protocolInfo") == 0) {
 				  sPrevLog = sLogOp;
 					continue;
@@ -250,7 +255,9 @@ std::string CUPnPSearch::BuildSQL(bool p_bCount)
           sOp = "in";
           
           #warning todo: use values from ContentDatabase.h
-				  if(sVal.compare("object.item.imageItem") == 0)
+				  /*if(sVal.compare("object.item") == 0)
+						
+					else*/ if(sVal.compare("object.item.imageItem") == 0)
 					  sVal = "(110, 111)";
 					else if(sVal.compare("object.item.audioItem") == 0)
 					  sVal = "(120, 121, 122)";	
