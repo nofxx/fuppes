db9d9593a77609d117666c29a381a4ba26cd67c0
u-voelkel <u-voelkel@92f15cf5-d3f9-0310-bf6d-83d55f1f8be4>
2010-05-21 08:00:39 +0000

    - missing files

---
+++ src/lib/Log.cpp
@@ -0,0 +1,154 @@
+#include "Log.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <stdarg.h>
+
+using namespace std;
+using namespace fuppes;
+
+std::string Log::senderToString(Log::Sender sender) // static
+{
+	switch(sender) {
+    case Log::unknown:
+      return "unknown";
+    case Log::http:
+      return "http";
+    case Log::soap:
+      return "soap";
+    case Log::gena:
+      return "gena";
+    case Log::ssdp:
+      return "ssdp";
+    case Log::fam:
+      return "fam";
+
+    case Log::contentdir:
+      return "contentdir";
+    case Log::contentdb:
+      return "contentdb";
+    case Log::sql:
+      return "sql";
+
+    case Log::plugin:
+      return "plugin";
+
+    case Log::config:
+      return "config";
+
+    case Log::hotplug:
+      return "hotplug";
+      
+    case Log::all:
+      return "all";
+		default:
+			return "unknown";
+	};	
+}
+
+Log::Sender Log::stringToSender(std::string sender) // static
+{
+  if(sender == "unknown")
+    return Log::unknown;
+  else if(sender == "http")
+    return Log::http;
+  else if(sender == "soap")
+    return Log::soap;
+  else if(sender == "gena")
+    return Log::gena;
+  else if(sender == "ssdp")
+    return Log::ssdp;
+  else if(sender == "fam")
+    return Log::fam;
+  
+  else if(sender == "contentdir")
+    return Log::contentdir;
+  else if(sender == "contentdb")
+    return Log::contentdb;
+  else if(sender == "sql")
+    return Log::sql;
+  
+  else if(sender == "plugin")
+    return Log::plugin;
+
+  else if(sender == "config")
+    return Log::config;
+
+  else if(sender == "hotplug")
+    return Log::hotplug;
+
+  else if(sender == "all")
+    return Log::all;
+  else
+    return Log::unknown;
+}
+
+
+Log* Log::m_instance = NULL;
+
+void Log::init() // static
+{
+  if(m_instance != NULL)
+    return;
+  
+  m_instance = new Log();
+  m_instance->m_logSenders |= Log::hotplug;
+}
+
+void Log::uninit() // static
+{
+  if(m_instance == NULL)
+    return;
+  
+  delete m_instance;
+  m_instance = NULL;
+}
+
+void Log::log(Log::Sender sender, Log::Level level, const std::string fileName, int lineNo, const char* format, ...) // static
+{
+  bool active = isActiveSender(sender);
+
+  if(!active) {
+    //cout << "Log sender: " << Log::senderToString(sender) << " not active" << endl;
+    return;
+  }
+  
+	va_list args;
+  va_start(args, format);
+	//CSharedLog::LogArgs(0, fileName, lineNo, format, args);
+	char buffer[8192 * 10];	
+ 	string out = "[" + Log::senderToString(sender) + "] ";
+  vsnprintf(buffer, sizeof(buffer) - 1, format, args);
+	va_end(args);
+	
+	cout << out << buffer << endl;
+}
+
+void Log::log(Log::Sender sender, Log::Level level, const std::string fileName, int lineNo, const std::string msg) // static
+{
+  bool active = isActiveSender(sender);
+
+  if(!active) {
+    //cout << "Log sender: " << Log::senderToString(sender) << " not active" << endl;
+    return;
+  }
+
+	string out = "[" + Log::senderToString(sender) + "] " + msg;
+	//CSharedLog::Log(0, fileName, lineNo, out);
+  cout << out << endl;
+}
+
+
+
+void Log::error(Log::Sender sender, Log::Level level, const std::string fileName, int lineNo, const char* format, ...) // static
+{
+	va_list args;
+  va_start(args, format);
+
+  char buffer[8192 * 10];	
+ 	string out = "[" + Log::senderToString(sender) + "] ";
+  vsnprintf(buffer, sizeof(buffer) - 1, format, args);
+	va_end(args);
+	
+	cout << out << buffer << endl;
+}
---
+++ src/lib/Log.h
@@ -0,0 +1,102 @@
+
+#ifndef _LOG_H
+#define _LOG_H
+
+#include <list>
+#include <string>
+
+namespace fuppes {
+
+	class Exception;
+	
+  class Log {
+
+		public:
+      Log() {
+        m_logSenders = 0;
+      }
+
+			enum Sender {
+				unknown    = 0,
+				http       = 1 << 0,
+        soap       = 1 << 1,
+        gena       = 1 << 2,
+				ssdp       = 1 << 3,
+				fam        = 1 << 4,
+        contentdir = 1 << 5,
+        contentdb  = 1 << 6,
+        sql        = 1 << 7,
+				plugin     = 1 << 8,
+        config     = 1 << 9,
+        hotplug    = 1 << 10,
+
+        all        = 
+          http | soap | gena | ssdp | fam | contentdir | contentdb | sql | plugin | config | hotplug
+			};
+		
+		  enum Level {
+		    none			= 0,
+		    normal		= 1,
+		    extended	= 2,
+		    debug			= 3
+		  };
+
+			static std::string senderToString(Log::Sender sender);
+			static Log::Sender stringToSender(std::string sender);
+      
+      static void init();
+      static void uninit();
+      
+		  static void log(Log::Sender sender, 
+		    Log::Level level, 
+		    const std::string fileName, 
+		    int lineNo,
+		    const char* format,
+		    ...);
+			
+			static void log(Log::Sender sender, 
+		    Log::Level level, 
+		    const std::string fileName, 
+		    int lineNo,
+		    const std::string msg);
+
+    static void error(Log::Sender sender, 
+		    Log::Level level, 
+		    const std::string fileName, 
+		    int lineNo,
+		    const char* format,
+		    ...);
+      
+      static bool isActiveSender(Log::Sender sender) {
+
+        return ((m_instance->m_logSenders & sender) == sender);
+        
+        /*std::list<Log::Sender>::iterator iter;
+        for(iter = m_instance->m_logSenders.begin(); 
+            iter != m_instance->m_logSenders.end(); 
+            iter++) {
+          if(*iter == sender)
+            return true;
+        }
+        return false;*/
+      }
+
+      static void addActiveSender(Log::Sender sender) {
+        if(sender == Log::unknown) return;
+        m_instance->m_logSenders |= sender; // even if they are the active one
+      }
+
+      static void removeActiveSender(Log::Sender sender) {
+        // and with the ones compliment of sender
+        m_instance->m_logSenders &= ~sender;
+      }
+
+    private:
+      static Log* m_instance;
+      //std::list<Log::Sender> m_logSenders;
+      int m_logSenders;
+  };
+
+}
+
+#endif // _LOG_H
