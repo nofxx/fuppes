cb8b59b8b6100ba687250c8ce0de19613b3b6fcd
Miguel González <migueljgonz@gmail.com>
2013-11-03 13:50:22 +0100

    Fixing constant types

--- src/plugins/metadata_libavformat.c
+++ src/plugins/metadata_libavformat.c
@@ -116,21 +116,21 @@ int fuppes_metadata_read(plugin_info* plugin, struct metadata_t* metadata)
     printf("codec %s*\n", pCodec->name);
     
 		switch(pStream->codec->codec_type) {
-			case CODEC_TYPE_VIDEO:
+			case AVMEDIA_TYPE_VIDEO:
 				metadata->type		= MD_VIDEO;
 				metadata->width 	= pStream->codec->width;
 				metadata->height	= pStream->codec->height;
 				set_value(metadata->video_codec, sizeof(metadata->video_codec), pCodec->name);
 				break;
-			case CODEC_TYPE_AUDIO:
+			case AVMEDIA_TYPE_AUDIO:
 				set_value(metadata->audio_codec, sizeof(metadata->audio_codec), pCodec->name);
 				if(metadata->type == MD_NONE) {
 					metadata->type = MD_AUDIO;
 				}
 				break;
-			case CODEC_TYPE_DATA:
+			case AVMEDIA_TYPE_DATA:
 				break;
-			case CODEC_TYPE_SUBTITLE:
+			case AVMEDIA_TYPE_SUBTITLE:
 				break;
 			default:
 				break;
